4375ed4c1ffcbda6e2379639ba77af7303108801
Alvinluipo <lualvin1234@gmail.com>
2016-01-05 23:49:19 -0500

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,8 @@
 # Write your #display_rainbow method here
+def display_rainbow(colors)
+  string=""
+  colors.each_with_index do |color, i|
+    string += "#{color.capitalize.split('').first}: #{colors[i]}, "
+  end
+  puts string.chomp(", ")
+end
