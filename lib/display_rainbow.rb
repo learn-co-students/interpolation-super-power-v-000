b1892353b249aa22151acf53fa0d087816c6692b
simque <simon.duncanson@gmail.com>
2016-01-09 10:22:25 -0500

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,18 @@
 # Write your #display_rainbow method here
+
+color_array = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+color_array2 = %w(red orange yellow green blue indigo violet) # refactored
+
+def display_rainbow(colors)
+  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+end
+
+def display_rainbow2(colors) # refactored
+  i = 0
+  new_array = []
+  colors.each do |color|
+    new_array << "#{colors["#{i}".to_i][0].upcase}: #{color}"
+    i += 1
+  end
+  puts new_array.join(", ")
+end
