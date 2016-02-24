17022c052c113c92f6cb20cc0331ef8523444a09
Ilapides <isaac.lapides@gmail.com>
2016-02-24 02:01:40 -0500

    done

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,21 @@
-# Write your #display_rainbow method here
+# # Write your #display_rainbow method here
+# require 'pry'
+
+# def display_rainbow(colors)
+#   initial_colors = ['R: ', 'O: ', 'Y: ', 'G: ', 'B: ', 'I: ', 'V: ']
+#   output = ''
+#   while (colors.size > 0) do
+#     output += initial_colors.shift + colors.shift + ', '
+#   end
+
+#   s_r = output.each_char.to_a
+#   s_r.pop(2)
+#   #binding.pry
+#   puts s_r.join('')
+# end
+
+def display_rainbow(colors)
+  if (colors.size > 0) 
+    puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+  end
+end
