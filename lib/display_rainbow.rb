bb88356baba40539d210638fee76123f4427e3c7
Marco Tsui <marcotsui2003@yahoo.com>
2016-04-04 11:12:20 -0400

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,8 @@
 # Write your #display_rainbow method here
+def display_rainbow(colors)
+  results = []
+  for i in 0..(colors.length - 1)
+    results[i]  = "#{colors[i][0].upcase}: #{colors[i]}"
+  end
+  puts results.join(', ')
+end
