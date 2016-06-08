333d8ab1dfd2a8e19ac9abb07ac7e801de370d55
Maurice Argoetti <maurice.argoetti@gmail.com>
2016-06-08 12:30:51 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,16 @@
 # Write your #display_rainbow method here
+
+def display_rainbow(colors)
+  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+end
+
+# puts display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+#
+#
+# def greeting(name)
+#   return "Hello #{name}"
+# end
+#
+# new_greeting = greeting("Peter") + "!"
+#
+# puts new_greeting
