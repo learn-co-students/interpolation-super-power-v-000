8b339b098173df4670c0cef38cdf946df2565c42
Karl Murray <karlmurray1138@gmail.com>
2016-11-08 23:55:09 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,13 @@
-# Write your #display_rainbow method here
+def display_rainbow(input)
+
+puts "R: #{input[0]}, O: #{input[1]}, Y: #{input[2]}, G: #{input[3]}, B: #{input[4]}, I: #{input[5]}, V: #{input[6]}"
+
+puts "The first color is: #{input[0]}"
+puts "The second color is: #{input[1]}"
+puts "The third color is: #{input[2]}"
+puts "The forth color is: #{input[3]}"
+puts "The fifth color is: #{input[4]}"
+puts "The sixth color is: #{input[5]}"
+puts "The seventh color is: #{input[6]}"
+
+end
