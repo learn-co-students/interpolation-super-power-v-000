121ee322deb9d7c1623c50f8e52d158f08497c91
Karl Murray <karlmurray1138@gmail.com>
2016-11-09 00:03:20 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,6 @@
 def display_rainbow(input)
 
 puts "R: #{input[0]}, O: #{input[1]}, Y: #{input[2]}, G: #{input[3]}, B: #{input[4]}, I: #{input[5]}, V: #{input[6]}"
-
 puts "The first color is: #{input[0]}"
 puts "The second color is: #{input[1]}"
 puts "The third color is: #{input[2]}"
