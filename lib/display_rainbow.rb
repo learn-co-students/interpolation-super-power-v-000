847d6f3190688d9df4f95e9685cbaf8c2df048e8
Yujun Sung <yujunsung@gmail.com>
2017-03-20 01:52:05 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,5 @@
 # Write your #display_rainbow method here
+def display_rainbow(array)
+  newArray = array
+  puts "R: #{newArray[0]}, O: #{newArray[1]}, Y: #{newArray[2]}, G: #{newArray[3]}, B: #{newArray[4]}, I: #{newArray[5]}, V: #{newArray[6]}"
+end
