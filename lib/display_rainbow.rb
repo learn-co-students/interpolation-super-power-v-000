ff5d2ebdcb320b2ddbcf7856d0a8ff830189bf2d
Will Sauer <will@heueso.com>
2017-03-23 00:12:09 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,13 @@
 # Write your #display_rainbow method here
+def display_rainbow (colors)
+  colors.each_index { |x|
+
+    if x<colors.length - 1
+      print colors[x][0].upcase + ': ' + colors[x] + ', '
+    elsif x = colors.length - 1
+      print "#{colors[x][0].upcase}: #{colors[x]}\n"
+
+    end
+  }
+
+end
