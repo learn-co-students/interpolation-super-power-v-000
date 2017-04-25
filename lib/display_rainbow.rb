ee83e2ac15b49c872aa182758b5cd79129b336c2
Sullivan Nolan <nolakat@gmail.com>
2017-04-25 02:56:14 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,16 @@
 # Write your #display_rainbow method here
+def display_rainbow(colors)
+puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+end
+
+ # rainbow = ["red", "blue", "green", "violet", "pink", "purple", "yellow"]
+ # display_rainbow(rainbow)
+
+#   colors.each do |color|
+#   firstLetter = color[0].capitalize
+#   if color == "violet"
+#      p "#{firstLetter}: #{color}\n ";
+#   else
+#     p "#{firstLetter}: #{color}, ";
+# end
+# end
