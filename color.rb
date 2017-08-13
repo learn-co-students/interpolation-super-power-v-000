33d7cdea729fac65f596bab301f8ba1e9a90b27e
Tuong Van Nguyen <tvan262@yahoo.com>
2017-08-13 09:04:00 +0000

    Done.

---
+++ color.rb
@@ -0,0 +1,7 @@
+colors = ['blue', 'orange', 'indigo', 'green', 'red', 'yellow', 'violet']# Write your #display_rainbow method here
+
+
+def display_rainbow(colors)
+  puts "R: #{colors[4]}, O: #{colors[1]}, Y: #{[5]}, G: #{colors[3]}, B: #{colors[0]}, I: #{colors[2]}, V: #{colors[6]}"
+  puts colors
+end
