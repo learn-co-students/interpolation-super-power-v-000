2c7053d3a2c2b7f002e37ed7365547cdc49e6786
Nadia Lee <miss.nadia.lee@gmail.com>
2017-09-28 19:48:53 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,5 @@
 def display_rainbow(colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"])
-  puts "R: #{color[0]}, #{color[1]}, #{color[2]}, #{color[3]}, #{color[4]}, #{color[5]}, #{color[6]}"
+  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
 end
+
+display_rainbow
