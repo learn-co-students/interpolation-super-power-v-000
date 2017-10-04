b2dbcc428250cdcd6bdc97e1990eff4430f0ded6
Charity Youngblood <youngblood.charity@gmail.com>
2017-10-04 19:53:04 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,14 +1,19 @@
 # Write your #display_rainbow method here
 # accepts an array of colors as its argument
-
 =begin
-
 $colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 $i = 0
-def rainbow_method(colors)
+def display_rainbow(colors)
     $colors.each do |capital_colors|
-     puts "#{$colors[$i].chars.first.capitalize}: #{$colors[$i]}, "
+     print "#{$colors[$i].chars.first.capitalize}: #{$colors[$i]}, "
         $i+=1
     end
 end
+
 =end
+
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+def display_rainbow(colors)
+  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}\n"
+end
+display_rainbow(colors)
