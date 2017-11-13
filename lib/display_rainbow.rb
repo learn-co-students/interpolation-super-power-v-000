fb012d20041326dc5541dbb725c7f6cbd4361255
James Dolan <jwdolan820@gmail.com>
2017-11-13 02:01:05 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,7 @@
-
 def display_rainbow(colors)
   puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
 end
+
+
+#Line 1: accepts array as an argument
+#Line 2-3: each element in the array is interpolated into the outputted string
