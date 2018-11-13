e08521628af3ee7520c5088c080c42e17b249a79
Ellen Pu <ellenpu251@gmail.com>
2018-11-13 23:37:20 +0000

    Done.

--- display_rainbow.rb
+++
@@ -1,4 +0,0 @@
-rainbow = ['red','orange','yellow','green','indigo', 'violet']
-def display_rainbow(rainbow)
-  puts "R: #{rainbow[0]}, O: #{rainbow[1]}, Y: #{rainbow[2]}, G: #{rainbow[3]}, #{rainbow[4]}, V: #{rainbow[5]} \n"
-end
